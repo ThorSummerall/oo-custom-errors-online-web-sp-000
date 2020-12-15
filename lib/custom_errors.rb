db1c87edb2f3f858bef4302f89fa55c0b0c484ec
Thor Summerall <thor.summerall@gmail.com>
2020-12-15 14:20:53 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -10,16 +10,12 @@ class Person
     person.partner = self
   end
 
-  class PartnerError < StandardError 
-    
+  class PartnerError < StandardError
+
   end
-  
+
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-class PartnerError < StandardError 
-  
-end
