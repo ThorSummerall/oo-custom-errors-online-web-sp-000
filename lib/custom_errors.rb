6b0d5defcd9a19b155719d22c265e9eb0911a4a0
Thor Summerall <thor.summerall@gmail.com>
2020-12-15 14:21:38 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,10 +7,10 @@ class Person
 
   def get_married(person)
     self.partner = person
-    if person.class != Person 
-      raise PartnerError 
-    else 
-      person.partner = self 
+    if person.class != Person
+      raise PartnerError
+    else
+      person.partner = self
     end
   end
 
